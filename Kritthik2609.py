8c47d4e41c3685c0ad1994c47f11573456a228ff
Kritthik2609 <kritthik2609@gmail.com>
2024-10-04 16:08:24 +0530

    Add files via upload

---
+++ Kritthik2609.py
@@ -0,0 +1,76 @@
+print("Hello World!")
+print("Hello \n World!")
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+         print("Five is greater than two!")
+if 5 > 2:
+             print("Five is greater than two!")
+
+
+if 5 > 2:
+    print("Five is greater than two!")
+    print("Five is greater than two!")
+
+
+    x = 5
+    y = "Leo"
+    print(x)
+    print(y)
+
+
+    x = str(3)
+    y = int(3)
+    z = float(3)
+    print(x,y,z)
+
+    x = 5
+    y = "Leo"
+    print(type(x))
+    print(type(y))
+
+
+    x = 1j
+    y = "Leo"
+    print(type(x))
+    print(type(y))
+
+
+    a = 5
+    A = "Leo"
+    print (a,A)
+
+
+    x, y, z = "Orange", "Banana", "Cherry"
+    print(x)
+    print(y)
+    print(z)
+
+    x= y= z = "Orange"
+    print(x)
+    print(y)
+    print(z)
+
+
+    x = "Python "
+    y = " is "
+    z = " awesome "
+    print(x + y + z)
+
+
+    a = "Hello "
+    b = "World"
+    c = a + b
+    print(c)
+
+
+    a = "Hello, World!"
+    print(a[0])
+
+
+    b = "Hello, World!"
+    print(b[:5])
