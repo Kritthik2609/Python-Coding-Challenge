2a9a3ae84301cb65796b76da87e0cddb9e908586
Kritthik2609 <kritthik2609@gmail.com>
2024-10-18 15:09:05 +0530

    Add files via upload

---
+++ StringDemo.py
@@ -0,0 +1,9 @@
+"""
+Author: Kritthik
+Date: 18-10-2024
+Description: Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+Output: World Everyone!
+"""
+string1 = "Hello, World!"
+string2 = string1[7:12]
+print(string2+ " Everyone!")
