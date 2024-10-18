5ac07c40eeb98a1f738f1ec066a3610ded94a491
Kritthik2609 <kritthik2609@gmail.com>
2024-10-18 14:25:44 +0530

    Add files via upload

---
+++ PythonCodingChallenge.py
@@ -0,0 +1,10 @@
+"""
+Author: Kritthik
+Date: 18-10-2024
+Description: Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Hello, John! You are 25 years old.
+"""
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("Hello,",name+"! You are ",age,"years old")
+
