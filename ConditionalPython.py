4b35e77e22c73cf0abe6bfd2bf5239e9f23c8fd8
Kritthik2609 <kritthik2609@gmail.com>
2024-10-25 14:39:10 +0530

    Add files via upload

--- ConditionalPython.py
+++ ConditionalPython.py
@@ -1,7 +1,6 @@
 """
 Author: Kritthik
 Date: 25-10-2024
-Description: Python program to find the largest of three numbers. The program should take three numbers as input from the user and determine which one is the largest. Use conditional statements to compare the numbers.
 Output: Enter the first number5
 Enter the second number10
 10  is greater than  5
