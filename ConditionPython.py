150bee6f93ede4a489a59c1f7d8498fe5e927bd8
Kritthik2609 <kritthik2609@gmail.com>
2024-10-25 15:11:16 +0530

    Add files via upload

---
+++ ConditionPython.py
@@ -0,0 +1,14 @@
+"""
+Author: Kritthik
+Date: 25-10-2024
+Output: Enter the number5
+5  is a positive number
+"""
+num = int(input("Enter the number"))
+if num>0:
+    print(num, " is a positive number")
+elif num<0:
+    print(num, "is a negative number")
+else:
+    print(num, "is zero")
+
