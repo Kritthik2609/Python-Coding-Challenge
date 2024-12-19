442adcdc523d5b46af8edf672c6aec874463ad0a
Kritthik2609 <kritthik2609@gmail.com>
2024-12-19 09:56:53 +0530

    Add files via upload

---
+++ DictionaryProgram.py
@@ -0,0 +1,25 @@
+"""
+Author: Kritthik
+Date: 19-12-2024
+Description: Dictionary Programming
+"""
+#Program 1
+
+# fruits = ["apple", "banana", "apple", "orange", "banana", "apple"]
+# fruits_count = {}
+#
+# for fruit in fruits:
+#     fruits_count[fruit] = fruits_count.get(fruit, 0) + 1
+#
+# print(fruits_count)
+
+#Program 2
+
+# string = "Python Python Java Java C C C# C# Django Django"
+# string_split = string.split(" ")
+# string_count = {}
+#
+# for strings in string_split:
+#     string_count[strings] = string_count.get(strings, 0) + 1
+#
+# print(string_count)
