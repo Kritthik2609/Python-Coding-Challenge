4906b7e3d89686027afdef2332b64b663480bfa9
Kritthik2609 <kritthik2609@gmail.com>
2024-12-19 09:57:30 +0530

    Add files via upload

---
+++ NumpyProgram.py
@@ -0,0 +1,20 @@
+"""
+Author: Kritthik
+Date: 19-12-2024
+Description: Numpy Programming
+"""
+import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5, 6])
+# print(arr)
+
+# mat = np.array([[1, 2, 3], [4, 5, 6]])
+# print(mat)
+
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print("Traversing a 2D array row by row: ")
+#
+# for row in arr:
+#     for element in row:
+#         print(element, end = " ")
+#     print()
